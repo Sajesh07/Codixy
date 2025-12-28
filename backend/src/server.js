e2ac745d19360ef443ef52e9399be93956de715b
Sajesh Atkare <sajeshatkare67@gmail.com>
2025-12-28 14:03:53 +0530

    added vercel.json

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import { ENV} from "./lib/env.js";
+import { ENV } from "./lib/env.js";
 import path from "path";
 
 dotenv.config();
@@ -8,22 +8,20 @@ dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 
-
-
 app.get("/test", (req, res) => {
-    res.send("Successfully connected to the server");
-})
+  res.send("Successfully connected to the server");
+});
 
 //make our app ready for deployment
-if(ENV.NODE_ENV === "production"){
-     app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-     app.get("/{*any}", (req, res)=> {
-        res.send(path.join(__dirname, "../frontend","dist", "index.html"));
-     })
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port ${ENV.PORT}`);
-})
+  console.log(`Server is running on port ${ENV.PORT}`);
+});
 export default app;
